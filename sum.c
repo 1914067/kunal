d4ab6aba501397143ba75cc23384eeac394b1165
1914067 <57886646+1914067@users.noreply.github.com>
2019-11-18 11:08:26 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+float x,y,z;
+
+printf("\nEnter The First Number: ");
+scanf("%f", &x);
+
+printf("\nEnter The Second Number: ");
+scanf("%f", &y);
+ z = x+y;
+printf("\nAnswer is: = %.3f", z);
+
+ return 0;
+
+}
